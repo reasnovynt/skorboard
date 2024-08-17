8ec59902831a22c7149ce6d9605b49828857167a
Reas Vyn <reasnov.app@gmail.com>
2024-08-18 02:46:48 +0700

    Deploy app to Github Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	base: "/skorboard/",
+	build: {
+		rollupOptions: {
+			input: {
+				main: "./index.html",
+			},
+		},
+	},
+	plugins: [react()],
+});
